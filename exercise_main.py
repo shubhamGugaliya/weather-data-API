444e19d3b884945333bd91b7b1cddfa5993a9b79
shubham gugaliya <ssg9404848801@gmail.com>
2025-02-09 14:40:15 +0530

    return temperature for any station and date

---
+++ exercise_main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("exercise_home.html")
+
+@app.route("/api/v1/<word>")
+def about(word):
+    temperature=23
+    return {'definition' : word.upper(),
+            'word' : word}
+
+if __name__=="__main__":
+    app.run(debug=True,port=5001)
